bc8730ddb92cf28ba611f80582c465c56b6a8e7b
avinash <avinashnanda008@gmail.com>
2024-07-27 02:44:51 +0530

    added: signup form

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import LoginForm from "@/components/LoginForm";
+import SignupForm from "@/components/SignupForm";
 
 export default function Home() {
   return (
---
+++ components/SignupForm.tsx
@@ -0,0 +1,55 @@
+import Image from "next/image";
+import Link from "next/link";
+
+const SignupForm = () => {
+  return (
+    <div className="border rounded-2xl space-y-4 bg-form-gradient w-[35%] p-10 border-[#CECECE]">
+      <div className="flex justify-center">
+        <h1 className="text-3xl font-semibold">
+          Welcome to <span className="text-[#4534AC]">Workflo!</span>
+        </h1>
+      </div>
+      <form className="space-y-3 mt-4">
+        <input
+          className="w-full bg-[#EBEBEB] outline-[#999999] p-2  h-10 rounded-lg"
+          type="text"
+          placeholder="Full name"
+        />
+        <input
+          className="w-full bg-[#EBEBEB] outline-[#999999] p-2  h-10 rounded-lg"
+          type="text"
+          placeholder="Your email"
+        />
+        <div className="relative">
+          <input
+            className="w-full bg-[#EBEBEB] outline-[#999999]  p-2 h-10 border rounded-lg"
+            type="text"
+            placeholder="Password"
+          />
+          <div className="absolute right-2 hover:cursor-pointer top-0 bottom-0 flex items-center">
+            <Image
+              src="/eye-icon.png"
+              width={20}
+              height={20}
+              alt="show password"
+            />
+          </div>
+        </div>
+
+        <button className="w-full h-10 border rounded-lg bg-form-button text-white">
+          Sign up
+        </button>
+      </form>
+      <div className="flex justify-center items-center">
+        <p className="text-sm font-normal text-[#606060]">
+          Already have an account?{" "}
+          <Link className="text-[#0054A1]" href="/">
+            Log in.
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default SignupForm;
