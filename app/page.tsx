c6f9f23276e11f94b0ef3a5b1bf34bee588c88f8
avinash <avinashnanda008@gmail.com>
2024-07-27 01:38:44 +0530

    style: added custom gradient bg

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center">
+    <main className="flex bg-custom-gradient min-h-screen flex-col items-center justify-center">
       <h1 className="text-xl font-medium">Hello World</h1>
       <p>This is where the magic happens!</p>
     </main>
