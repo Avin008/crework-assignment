4aec0331576532453ea0a4b16d4d1781285f8343
avinash <avinashnanda008@gmail.com>
2024-07-27 19:07:52 +0530

    added: todo categories

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -55,6 +55,36 @@ const Dashboard = () => {
         <section>
           <TaskActions />
         </section>
+        <section className="grid grid-cols-4 gap-3 bg-white">
+          <div className="">
+            <div className="flex justify-between items-center">
+              <h1 className="text-[#555555] font-normal text-base">To do</h1>
+              <Image src="/stairs.png" width={24} height={24} alt="" />
+            </div>
+          </div>
+          <div className="">
+            <div className="flex justify-between items-center">
+              <h1 className="text-[#555555] font-normal text-base">
+                In progress
+              </h1>
+              <Image src="/stairs.png" width={24} height={24} alt="" />
+            </div>
+          </div>
+          <div className="">
+            <div className="flex justify-between items-center">
+              <h1 className="text-[#555555] font-normal text-base">
+                Under review
+              </h1>
+              <Image src="/stairs.png" width={24} height={24} alt="" />
+            </div>
+          </div>
+          <div className="">
+            <div className="flex justify-between items-center">
+              <h1 className="text-[#555555] font-normal text-base">Finished</h1>
+              <Image src="/stairs.png" width={24} height={24} alt="" />
+            </div>
+          </div>
+        </section>
       </section>
     </main>
   );
