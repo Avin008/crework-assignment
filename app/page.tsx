b27be92292fdad08929b9575f84bff1bef31e33c
avinash <avinashnanda008@gmail.com>
2024-07-29 18:52:04 +0530

    fix: next-server warning

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,20 @@
 "use client";
 import { useAuthStore } from "@/store/useAuthStore";
 import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Home() {
   const router = useRouter();
 
   const authStatus = useAuthStore((store) => store.authStatus);
 
-  if (authStatus) {
-    router.push("/dashboard");
-  } else {
-    router.push("/login");
-  }
+  useEffect(() => {
+    if (authStatus) {
+      router.push("/dashboard");
+    } else {
+      router.push("/login");
+    }
+  }, [authStatus]);
+
   return null;
 }
